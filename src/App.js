51ed3710074f6933b4989f970dbcfc307b2af58d
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-11-11 23:53:08 +0530

    React: Hooks, useState, Functional Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import ChildComponent from './components/ChildComponent'
+import Count from './components/Count'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Count />
+      {/* <ChildComponent hobby='Swimming' />
+			<ChildComponent hobby='Singing' />
+			<ChildComponent hobby='Dancing' />
+			<ChildComponent hobby='Reading Books' />
+			<ChildComponent hobby='Sleeping' /> */}
     </div>
   );
 }
