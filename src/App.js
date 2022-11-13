2df08f52b41afabea06179c79d7d9598953795fb
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-11-13 13:45:18 +0530

    Form Controlled Components, Conditional Rendering, Loop Rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 // import ChildComponent from './components/ChildComponent'
-import Count from './components/Count'
+// import Count from './components/Count'
+// import Form from './components/Form'
+// import Student from './components/Student'
+import Loop from './components/Loop'
 
 const App = () => {
   return (
     <div className="App">
-			<Count />
+			<Loop />
+			{/* <Student
+				firstName='Srajan'
+				lastName='Sen'
+				batchNumber={12}
+				likesIceCream={true}
+			/> */}
+			{/* <Form /> */}
+			{/* <Count /> */}
       {/* <ChildComponent hobby='Swimming' />
 			<ChildComponent hobby='Singing' />
 			<ChildComponent hobby='Dancing' />
